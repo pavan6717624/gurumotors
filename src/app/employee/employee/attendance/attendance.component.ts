847245c1f25f032552d833316fc3292cb0dd2029
pavan6717624 <pvn.kumar4@gmail.com>
2023-06-02 18:46:54 +0530

    deploy

--- src/app/employee/employee/attendance/attendance.component.ts
+++ src/app/employee/employee/attendance/attendance.component.ts
@@ -8,14 +8,31 @@ import { Component, OnInit } from '@angular/core';
 export class AttendanceComponent implements OnInit {
 
   constructor() { }
-  attendance: string='';
+  attendance: string='Present';
+  date: Date=new Date();
+  currDate: Date=new Date();
+  
+  day: string='Full Day';
+  punchInTime: Date=new Date();
+  punchOutTime: Date=new Date();
+  lunchInTime: Date=new Date();
+  lunchOutTime:Date=new Date();
 
   stateOptions: any[] = [
     { label: 'Full Day', value: 'Full Day' },
     { label: 'Half Day', value: 'Half Day' }
 ];
 
+
+attCheck()
+{
+  if(this.attendance!='Present')
+  {
+    this.day='Full Day';
+  }
+}
   ngOnInit(): void {
+   
   }
 
 }
